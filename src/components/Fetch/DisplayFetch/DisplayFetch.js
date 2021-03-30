32076469a344084a2dfbf659d6de5fe240693454
Shanec92 <shanecox@MacBook-Air.local>
2021-03-30 19:55:34 -0400

    cleanup

---
+++ src/components/Fetch/DisplayFetch/DisplayFetch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayFetch = props => {
+    return (
+        <div>
+            <img src={props.url} alt='Dog pic' />
+        </div>
+    )
+}
+
+export default DisplayFetch;
---
+++ src/components/Functional/FunctionalComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const FunctionalComponent = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+
+export default FunctionalComponent;
